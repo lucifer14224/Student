2cf4dacb2ad20fe814dfe06af599de4ebb3dcd1e
PipatpongTH <88373064+lucifer14224@users.noreply.github.com>
2021-10-16 10:41:14 +0700

    Add files via upload

---
+++ AppDataStudent/AppDataStudent/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace AppDataStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.dtBirth = new System.Windows.Forms.DateTimePicker();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSName);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtWeight);
+            this.groupBox1.Controls.Add(this.dtBirth);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtid);
+            this.groupBox1.Location = new System.Drawing.Point(29, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(774, 261);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "รายการข้อมูลนักศึกษา";
+            // 
+            // txtSName
+            // 
+            this.txtSName.Location = new System.Drawing.Point(163, 120);
+            this.txtSName.Name = "txtSName";
+            this.txtSName.Size = new System.Drawing.Size(264, 22);
+            this.txtSName.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(87, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "น้ำหนัก";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "วัน เดือน ปี เกิด";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "รหัสนักศึกษา";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(87, 123);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(53, 17);
+            this.label.TabIndex = 6;
+            this.label.Text = "นามสกุล";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ชื่อ";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(163, 175);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtWeight.TabIndex = 4;
+            // 
+            // dtBirth
+            // 
+            this.dtBirth.Location = new System.Drawing.Point(163, 147);
+            this.dtBirth.Name = "dtBirth";
+            this.dtBirth.Size = new System.Drawing.Size(200, 22);
+            this.dtBirth.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(163, 90);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(264, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(163, 62);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(264, 22);
+            this.txtid.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnEdit);
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Location = new System.Drawing.Point(845, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(234, 614);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "การดำเนินการทั้งหมด";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(38, 502);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(151, 66);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "ออกจากโปรแรม";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(38, 267);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(151, 66);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "ลบ";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(38, 196);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(151, 66);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "แก้ไข";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(38, 124);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(151, 66);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "บันทึก";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(38, 52);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(151, 66);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "เพิ่มใหม่";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvShow);
+            this.groupBox3.Location = new System.Drawing.Point(29, 279);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(773, 347);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "ข้อมูลนักศึกษาทั้งหมด";
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Location = new System.Drawing.Point(6, 21);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.RowHeadersWidth = 51;
+            this.dgvShow.RowTemplate.Height = 24;
+            this.dgvShow.Size = new System.Drawing.Size(761, 311);
+            this.dgvShow.TabIndex = 0;
+            this.dgvShow.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvShow_CellMouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 650);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "หน้าจอการจัดการข้อมูลนักศึกษา";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.DateTimePicker dtBirth;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.TextBox txtSName;
+    }
+}
+
---
+++ AppDataStudent/AppDataStudent/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace AppDataStudent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string StrConn = "Provider = Microsoft.ACE.OLEDB.12.0;data source = C:/Users/class/Desktop/Database/db_student.accdb";
+        public string sql;
+        OleDbConnection Conn = new OleDbConnection();
+        OleDbDataAdapter da;
+        DataSet ds = new DataSet();
+        bool IsFind = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Conn.State == ConnectionState.Open)
+            {
+                Conn.Close();
+            }
+            Conn.ConnectionString = StrConn;
+            Conn.Open();
+
+            ShowAllStudent();
+            FormatDataStudent();
+            ClearAllStudent();
+        }
+
+        private void ShowAllStudent()
+        {
+            string sqlStu = "select * from tb_histstudent";
+            if (IsFind == true)
+            {
+                ds.Tables["tb_hisstudent"].Clear();
+            }
+            da = new OleDbDataAdapter(sqlStu, Conn);
+            da.Fill(ds, "tb_histstudent");
+            if (ds.Tables["tb_histstudent"].Rows.Count != 0){
+                IsFind = true;
+                dgvShow.DataSource = ds.Tables["tb_histstudent"];
+            }
+            else
+            {
+                IsFind = false;
+            }
+        }
+
+        private void FormatDataStudent()
+        {
+            DataGridViewCellStyle cs = new DataGridViewCellStyle();
+            cs.Font = new Font("Ms Sans Serif", 9, FontStyle.Regular);
+            dgvShow.ColumnHeadersDefaultCellStyle = cs;
+            dgvShow.Columns[0].HeaderText = "รหัสนักศึกษา";
+            dgvShow.Columns[1].HeaderText = "ชื่อ";
+            dgvShow.Columns[2].HeaderText = "นามสกุล";
+            dgvShow.Columns[3].HeaderText = "วัน เดือน ปี เกิด";
+            dgvShow.Columns[4].HeaderText = "น้ำหนัก";
+
+            dgvShow.Columns[0].Width = 80;
+            dgvShow.Columns[1].Width = 120;
+            dgvShow.Columns[2].Width = 120;
+            dgvShow.Columns[3].Width = 100;
+            dgvShow.Columns[4].Width = 80;
+        }
+
+        private void dgvShow_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex == dgvShow.Rows.Count - 1)
+            {
+                return;
+            }
+            try
+            {
+                txtid.Text = dgvShow.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtName.Text = dgvShow.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtSName.Text = dgvShow.Rows[e.RowIndex].Cells[2].Value.ToString();
+                dtBirth.Value = Convert.ToDateTime(dgvShow.Rows[e.RowIndex].Cells[3].Value);
+                txtWeight.Text = dgvShow.Rows[e.RowIndex].Cells[4].Value.ToString();
+                
+            }
+            catch
+            {
+                MessageBox.Show("เกิดข้อผิดพลาด");
+            }
+        }
+
+        private void ClearAllStudent()
+        {
+            txtid.Text = "";
+            txtName.Text = "";
+            txtSName.Text = "";
+            dtBirth.Value = DateTime.Now;
+            txtWeight.Text = "";
+            txtid.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ClearAllStudent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtid.Text == "")
+            {
+                MessageBox.Show("กรุณากรอกข้อมูลให้ครบ","ผิดพลาด");
+                txtid.Focus();
+                return;
+            }
+            string sqlAdd = "";
+            OleDbCommand ComAdd = new OleDbCommand();
+            try
+            {
+                if (MessageBox.Show("คุณต้องการเพิ่มข้อมูลใช่หรือไม่ ?","คำเตือน",MessageBoxButtons.YesNo,MessageBoxIcon.Question)== DialogResult.Yes)
+                {
+                    sqlAdd = "insert into tb_histstudent(stu_id,stu_name,stu_surname,stu_bdate,stu_weight)value('"
+                        + txtWeight.Text + "','" 
+                        + txtName.Text + "','" 
+                        + txtSName.Text + "','" 
+                        + dtBirth.Value + "','" 
+                        + txtWeight.Text + "')";
+
+                    if(Conn.State == ConnectionState.Open)
+                    {
+                        Conn.Close();
+                    }
+                }
+                Conn.ConnectionString = StrConn;
+                Conn.Open();
+
+                ComAdd.CommandType = CommandType.Text;
+                ComAdd.CommandText = sqlAdd;
+                ComAdd.Connection = Conn;
+                ComAdd.ExecuteNonQuery();
+
+                MessageBox.Show("ได้บันทึกข้อมูลเรียบร้อยแล้ว","ผลการดำเนินการ");
+                ClearAllStudent();
+                ShowAllStudent();
+            }
+            catch
+            {
+                MessageBox.Show("ไม่สามารถติดต่อฐานข้อมูลได้","ผิดพลาด");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("กรุณากรอกข้อมูลที่จะแก้ไขให้ครบ", "ผิดพลาด");
+                txtName.Focus();
+                return;
+            }
+            string sqlEdit = "";
+            OleDbCommand ComEdit = new OleDbCommand();
+            try
+            {
+                if (MessageBox.Show("คุณต้องการแก้ไขข้อมูลใช่หรือไม่ ?", "ยืนยัน", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    sqlEdit = "update tb_histstudent set stu_name = '" + txtName.Text + "',stu_surname = '" + txtSName.Text + "',stu_bdate = '" + dtBirth.Value + "',stu_weight = '" 
+                        + txtWeight.Text+ "',where stu_id ='" + txtid.Text + "'";
+
+                    if (Conn.State == ConnectionState.Open)
+                    {
+                        Conn.Close();
+                    }
+                }
+                Conn.ConnectionString = StrConn;
+                Conn.Open();
+
+                ComEdit.CommandType = CommandType.Text;
+                ComEdit.CommandText = sqlEdit;
+                ComEdit.Connection = Conn;
+                ComEdit.ExecuteNonQuery();
+
+                MessageBox.Show("แก้ไขข้อมูลเรียบร้อยแล้ว", "ผลการดำเนินการ");
+                ClearAllStudent();
+                ShowAllStudent();
+            }
+            catch
+            {
+                MessageBox.Show("ข้อมูลผิดพลาด", "ผิดพลาด");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtid.Text == "")
+                {
+                    MessageBox.Show("กรุณาเลือกข้อมูลที่จะลบ", "ผิดพลาด");
+                    return;
+                }
+                string sqlDel = "delete from tb_histstudent where stu_id = '" + txtid.Text + "'";
+                if (Conn.State == ConnectionState.Open)
+                {
+                    Conn.Close();
+                }
+
+                Conn.ConnectionString = StrConn;
+                Conn.Open();
+                OleDbCommand ComDel = new OleDbCommand(sqlDel, Conn);
+                if (MessageBox.Show("คุณต้องการลบข้อมูลนี้ใช่หรือไม่ ?", "ยืนยัน", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                {
+
+                    ComDel.ExecuteNonQuery();
+                    MessageBox.Show("ลบข้อมูลเรียบร้อยแล้ว", "ผลการดำเนินการ");
+                    ClearAllStudent();
+                    ShowAllStudent();
+                }
+             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("คุณต้องการจบการทำงานหรือไม่?", "Exit ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                MessageBox.Show("จบการทำงานครับ", "End");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("คุณยังคงทำงานต่อไป","Continue");
+            }
+        }
+       
+    }
+}
